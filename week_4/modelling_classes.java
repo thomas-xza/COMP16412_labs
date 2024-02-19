dacc14e70b17628e1367970fa0adaaf0b24440a4
main <thomas.prior@student.manchester.ac.uk>
2024-02-19 16:03:13 +0000

    Some work on modelling classes, week 4 lab

---
+++ week_4/modelling_classes.java
@@ -0,0 +1,23 @@
+public class Animal {
+
+    private int avg_weight;
+    private int avg_height;
+    private int avg_width;
+    private bool prey;
+    private String name;
+
+    public void Animal(int avg_we, int avg_h, int avg_wi, bool prety) {
+	this.avg_weight = avg_we;
+	//    etc...
+    }
+}
+
+public class Chordata {
+    private int subphyla_type;
+}
+
+public class Annelida {
+    private int segments;
+}
+
+public class 
